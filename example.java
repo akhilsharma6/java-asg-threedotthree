18455053f1ecfa96d6f351aab276e24d16f62700
akhilsharma6 <akhilsharma69@yahoo.co.in>
2016-11-14 21:22:13 +0530

    Assignment 3.3 complete

---
+++ example.java
@@ -0,0 +1,10 @@
+/* Program to understand variable initialization and error finding */
+class example {
+	int x; int y;	//declare integer variable
+	String name;	//declar string variable
+	public static void main(String[] args){
+	example pnt = new example();	//create object pnt
+	System.out.println("pnt is " + pnt.name + " " + pnt.x + " " + pnt.y);	//print
+}}
+
+// Output: d. pnt is null 0 0
